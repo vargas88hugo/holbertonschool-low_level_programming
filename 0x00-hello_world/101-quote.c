4315615565754ad81f722f735c6a263c509cf806
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-19 21:34:12 +0000

    file 101 added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 Standard Error
+ */
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
