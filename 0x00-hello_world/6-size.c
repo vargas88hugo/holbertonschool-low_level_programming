9e35b5c6547e49d833ffcf607342bd9ca3daaf41
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-19 19:53:34 +0000

    file 6 added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ int i;
 long int li;
 long long int lli;
 float f;
-
 printf("Size of a char: %lu byte(s)\n", sizeof(c));
 printf("Size of an int: %lu byte(s)\n", sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", sizeof(li));
