bc644a3aab73a8fa7b9b511a01231db1f9627644
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-19 21:46:31 +0000

    file 101 added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,7 +7,9 @@
  */
 int main(void)
 {
-	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+        char str[] = {"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"};
+
+	write(2, str, 60);
 
 	return (1);
 }
