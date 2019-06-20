e700bf91c06069a96d1db166876bc3e19dbc41e2
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-20 21:34:17 +0000

    file 10 added

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -7,5 +7,33 @@
  */
 int main(void)
 {
-	int n = 
+	int n = 48, m;
+
+	while (n <= 57)
+	{
+
+		m = 48;
+
+	        while (m <= 57)
+		{
+			putchar(n);
+
+			putchar(m);
+
+		        if(n != 57 || m != 57)
+			{
+				putchar(',');
+
+				putchar(' ');
+			}
+
+			m++;
+		}
+
+		n++;
+	}
+
+	putchar(10);
+
+	return (0);
 }
