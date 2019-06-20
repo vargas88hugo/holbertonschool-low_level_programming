a2b06df3e6fabc1cf7247c62e1a6fc72cdb9df3f
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-20 18:00:30 +0000

    file 7 added

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+
+		n--;
+	}
+
+	putchar(10);
+
+	return (0);
+}
