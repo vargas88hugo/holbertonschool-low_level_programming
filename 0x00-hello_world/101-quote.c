5a7a402159a8fe631d7bdebb8d20ae74ae9244a1
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-20 01:50:48 +0000

    file 101 fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,10 @@ int main(void)
 {
 	char str[] = {"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"};
 
-	write(2, str, 60);
+        for (int i = 0; i < 60; i++)
+	{
+	        putchar(stderr, text[i]);
+	}
 
 	return (1);
 }
