4014468ead4258a19213d9e27b2784c2f097c015
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-20 23:03:12 +0000

    file 100 added

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 48, c = 48, m;
+
+	while (n <= 57)
+	{
+		m = 48;
+
+		while (m <= 57)
+		{
+
+			if (m > c && m != n)
+			{
+				putchar(n);
+
+				putchar(m);
+
+				if (n != 57 || m != 57)
+				{
+					putchar(',');
+
+					putchar(' ');
+				}
+			}
+
+			m++;
+		}
+
+		n++;
+
+		c++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
