0b0c6b54e8eef42704d9cfd3f4f6e11cfd1f7c21
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-21 00:13:46 +0000

    file 101 fixed

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,40 +12,28 @@ int main(void)
 	while (n <= 57)
 	{
 		m = 48;
-
 		while (m <= 57)
 		{
 			l = 48;
-
 			while (l <= 57)
 			{
-
 				if (l > m && m > n)
 				{
 					putchar(n);
-
 					putchar(m);
-
 					putchar(l);
-
 					if (n != 55 || m != 56 || l != 57)
 					{
 						putchar(',');
-
 						putchar(' ');
 					}
 				}
-
 				l++;
 			}
-
 			m++;
 		}
-
 		n++;
 	}
-
 	putchar(10);
-
 	return (0);
 }
