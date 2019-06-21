167b30ade671f3c3c7e038531a867fafcd93dd6a
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-21 01:07:29 +0000

    file 102 added

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 57; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
