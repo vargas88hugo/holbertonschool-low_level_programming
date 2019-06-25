585dfd878f565ed019c6691e955a1e77e63913d1
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-25 13:03:12 +0000

    file 11 fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - Function that prints natural numbers from n to 98
@@ -7,77 +8,37 @@
  */
 void print_to_98(int n)
 {
-	int i;
 
 	if (n < 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (; n <= 98; n++)
 		{
-
-			if (i < 10 && i >= 0)
+			if (n != 98)
 			{
-				_putchar(i + '0');
-			}
-			else if (i >= 10)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
+				printf("%d, ", n);
 			}
 			else
 			{
-				if (i > -10)
-				{
-					_putchar('-');
-					_putchar(((i % 10) * -1) + '0');
-				}
-				else
-				{
-					_putchar('-');
-					_putchar(((i / 10) * -1) + '0');
-					_putchar(((i % 10) * -1) + '0');
-				}
-			}
-
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
+				printf("%d\n", n);
 			}
 		}
-		_putchar(10);
 	}
 	else if (n == 98)
 	{
-		_putchar('9');
-		_putchar('8');
-		_putchar(10);
+		printf("%d\n", n);
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (; n >= 98; n--)
 		{
-			if (i < 10 && i > 0)
+			if (n != 98)
 			{
-				_putchar(i + '0');
-			}
-			else if (i >= 10 && i < 100)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
+				printf("%d, ", n);
 			}
 			else
 			{
-				_putchar((i / 100) + '0');
-				_putchar(((i / 10) % 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
+				printf("%d\n", n);
 			}
 		}
-		_putchar(10);
 	}
 }
