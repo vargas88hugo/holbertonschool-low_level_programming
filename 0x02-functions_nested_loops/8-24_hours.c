327937a8256b07723b609621974c3e9b2ae13147
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-25 11:52:21 +0000

    file 8 fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,25 +8,23 @@ void jack_bauer(void)
 {
 	char a, b, c, d;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = '0'; a <= '2'; a++)
 	{
 		for (b = '0'; b <= '9'; b++)
 		{
-			for (c = '0'; c <= '9'; c++)
+			for (c = '0'; c <= '5'; c++)
 			{
 				for (d = '0'; d <= '9'; d++)
 				{
-					if (a <= '2' && ((b <= '9' && a != '2') || (b <= '3' && a == '2')))
+
+					if (((b <= '9' && a != '2') || (b <= '3' && a == '2')))
 					{
-						if (c <= '5')
-						{
-							_putchar(a);
-							_putchar(b);
-							_putchar(':');
-							_putchar(c);
-							_putchar(d);
-							_putchar(10);
-						}
+						_putchar(a);
+						_putchar(b);
+						_putchar(':');
+						_putchar(c);
+						_putchar(d);
+						_putchar(10);
 					}
 				}
 			}
