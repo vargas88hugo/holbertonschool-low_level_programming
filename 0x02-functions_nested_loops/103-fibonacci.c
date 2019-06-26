bcdb59e72e22c3821fd8bbb20edc59922f3084e6
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-26 02:29:27 +0000

    file 103 fixed

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,31 +8,20 @@
 int main(void)
 {
 	int i;
-	long int a = 1, b = 2, c;
-
-	c = 0;
-
-	printf("1, 2");
+	long int a = 1, b = 2, c = 0, sum = 0;
 
 	while (c <= 4000000)
 	{
-		if (c < 3)
-		{
-			c = a + b;
-		}
-		else
-		{
-			a = b;
-			b = c;
-			c = a + b;
-		}
+		c = a + b;
+		a = b;
+		b = c;
 
 		if (c <= 4000000 && c % 2 == 0)
 		{
-		printf(", %ld", c);
+			sum += c;
 		}
 	}
-	putchar(10);
+	printf("%ld\n", sum);
 
 	return (0);
 }
