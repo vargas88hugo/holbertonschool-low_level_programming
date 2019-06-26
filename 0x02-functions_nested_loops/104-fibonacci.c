2081a342927460f90b84123fef9378e7f95d3e9d
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-26 02:57:39 +0000

    file 104 added

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	long int a = 1, b = 2, c;
+
+	for (i = 1; i <= 98; i++)
+	{
+		if (i <= 2)
+		{
+			c = i;
+		}
+		else
+		{
+			c = a + b;
+			a = b;
+			b = c;
+		}
+		printf("%ld", c);
+		if (i < 50)
+		{
+			printf(", ");
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
