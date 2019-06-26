ab7b2c8a5f96e9c36e38396cd0f257ce931615a3
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-26 02:19:05 +0000

    file 103 added

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	long int a = 1, b = 2, c;
+
+	c = 0;
+
+	printf("1, 2");
+
+        while (c <= 4000000)
+	{
+		if (c < 3)
+		{
+			c = a + b;
+		}
+		else
+		{
+			a = b;
+			b = c;
+			c = a + b;
+		}
+
+		if (c <= 4000000 && c % 2 == 0)
+		{
+		printf(", %ld", c);
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
