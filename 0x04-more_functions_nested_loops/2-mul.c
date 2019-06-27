c8ccd9f0b4cb278afcbc326df77d9af78e15a52d
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-27 16:22:56 +0000

    files added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - Function that multiplies two integers
+ * @a: Number to be multiply
+ * @b: Number to be multiply
+ * Return: arithmetic operation
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - Function that prints 10 times the numbers, from 0 to 14
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 0 && j <= 9)
+				_putchar(j + '0');
+			else
+			{
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+			}
+		}
+		_putchar(10);
+	}
+}
