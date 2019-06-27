9d7533a5ed5ea8ab27a5bb968d71e06bf992f0f6
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-27 23:16:20 +0000

    file 101 fixed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,45 +6,31 @@
  */
 void print_number(int n)
 {
-        unsigned int i = n;
+	unsigned int i = n;
 
-	if ((n >= 0 && n <= 9) || (n < 0 && n >= -9))
+	if (n < 0)
 	{
-		if (n < 0)
-		{
-			_putchar('-');
-			i = n * -1;
-		}
+		_putchar('-');
+		i = n * -1;
+	}
+
+	if (i <= 9)
+	{
 		_putchar(i + '0');
 	}
-	else if ((n >= 10 && n <= 99) || (n <= -10 && n >= -99))
+	else if (i >= 10 && i <= 99)
 	{
-		if (n <= -10)
-		{
-			_putchar('-');
-			i = n * -1;
-		}
 		_putchar((i / 10) + '0');
 		_putchar((i % 10) + '0');
 	}
-	else if ((n >= 100 && n <= 999) || (n <= -100 && n >= -999))
+	else if (i >= 100 && i <= 999)
 	{
-		if (n <= -100)
-		{
-			_putchar('-');
-			i = n * -1;
-		}
 		_putchar((i / 100) + '0');
 		_putchar(((i / 10) % 10) + '0');
 		_putchar((i % 10) + '0');
 	}
-	else if ((n >= 1000 && n <= 9999) || (n <= -1000 && n >= -9999))
+	else if (i >= 1000 && i <= 9999)
 	{
-		if (n <= -1000)
-		{
-			_putchar('-');
-			i = n * -1;
-		}
 		_putchar((i / 1000) + '0');
 		_putchar(((i / 100) % 10) + '0');
 		_putchar(((i / 10) % 10) + '0');
