1e3060d0944a4bbf4d9caf8a2aaf5a41f664185c
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-27 17:17:43 +0000

    file 10 fixed

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,22 +6,25 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
         if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < i; j++)
+			for (j = 0; j < size - i - 1; j++)
 			{
 				_putchar(' ');
 			}
+			for (k = 0; k <= i; k++)
+			{
 			_putchar('#');
+			}
 			_putchar(10);
 		}
 	}
 	else
 	{
-
+		_putchar(10);
 	}
 }
