dbcdf21ecfbb606fda7eed49e42c90c74603ebba
Hugo Vargas <vargas88hugo@gmail.com>
2019-06-27 17:06:54 +0000

    file 5 fixed

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,7 +12,7 @@ void print_triangle(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 1; j < i; j++)
+			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
 			}
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,19 +5,20 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, n;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 0 && j <= 9)
-				_putchar(j + '0');
+				n = (j + '0');
 			else
 			{
+				n = ((j % 10) + '0');
 				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
 			}
+			_putchar(n);
 		}
 		_putchar(10);
 	}
