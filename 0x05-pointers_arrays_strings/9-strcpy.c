19b32fde77529d629f5fd2b1f4a75d5f45d630df
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-01 21:49:22 +0000

    file 9 added

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - Function that copies a string
+ * @dest: copy string
+ * @src: string to be copied
+ * Return: value the pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	
+}
