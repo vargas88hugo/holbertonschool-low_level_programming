080a74d863424d08464c029b9d58191e173b7019
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-03 00:02:08 +0000

    file 100 added

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,5 +7,28 @@
  */
 int _atoi(char *s)
 {
+	int i, j, n = 0, count = 0, m = 0;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			m++;
+			for (j = i; j >= 0; j--)
+			{
+				if (s[j] == '-' && m == 1)
+					count++;
+			}
+
+			n = n * 10 + (s[i] - '0');
+
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+		}
+	}
+
+	if (count % 2 == 0)
+		return (n);
+	else
+		return (-n);
 }
