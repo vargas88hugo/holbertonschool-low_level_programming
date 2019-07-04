599d52e8e0a2d3c42528a23ac8c199c90e732d58
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-04 21:14:32 +0000

    file 4 added

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - Function that reverses the content of an array
+ * @a: array
+ * @n: number of elements
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, tmp;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			tmp = *(a + j);
+			*(a + j) = *(a + j - 1);
+			*(a + j - 1) = tmp;
+		}
+	}
+}
