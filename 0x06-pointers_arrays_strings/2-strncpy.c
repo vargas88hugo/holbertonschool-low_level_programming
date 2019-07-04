7bf5cf86fd235c572b61f8f71279638a53ee8b5c
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-04 18:36:18 +0000

    file 2 added

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,35 +9,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j = 0, a = 0, c = 0;
+	int i;
 
-	while (1)
-	{
-		if (*(src + j) == '\0')
-			break;
-		j++;
-	}
-
-	while (1)
-	{
-		if (*(dest + a) == '\0')
-			break;
-		a++;
-	}
-
-	if (n >= j)
-		c++;
-	else
-		j = n;
-
-	for (i = 0; i < j; i++)
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
 		*(dest + i) = *(src + i);
 
-	if (c > 0)
-	{
-		for (; *(dest + i) != 0; i++)
-			*(dest + i) = '\0';
-	}
+	for (; i < n; i++)
+		*(dest + i) = '\0';
 
 	return (dest);
 }
