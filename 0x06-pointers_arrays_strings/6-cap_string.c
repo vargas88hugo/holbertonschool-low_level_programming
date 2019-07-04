0f97311438b187352a6f17af97cf8374d022c649
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-04 22:08:01 +0000

    file 6 added

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * cap_string - Function that capitalizes all words of string
+ * @c: string to be capitalized
+ * Return: string capitalized
+ */
+char *cap_string(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] == ' ' || c[i] == '\t' || c[i] == '\n' || c[i] == ',')
+		{
+			if (c[i + 1] >= 'a' && c[i + 1] <= 'z')
+				c[i + 1] -= 32;
+		}
+
+		if (c[i] == ';' || c[i] == '.' || c[i] == '!' || c[i] == '?')
+		{
+			if (c[i + 1] >= 'a' && c[i + 1] <= 'z')
+				c[i + 1] -= 32;
+		}
+
+		if (c[i] == '"' || c[i] == '(' || c[i] == ')' || c[i] == '{')
+			if (c[i + 1] >= 'a' && c[i + 1] <= 'z')
+				c[i + 1] -= 32;
+
+		if (c[i] == '}')
+			if (c[i + 1] >= 'a' && c[i + 1] <= 'z')
+				c[i + 1] -= 32;
+	}
+
+	return (c);
+}
