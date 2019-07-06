efdf1cf55d2a8fc895d3e1fdf8f002d060b8cf6c
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-06 17:33:30 +0000

    file 102 fixed

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -11,62 +11,39 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, x, y, z, sum, tmp;
+	int i, j, x, y, z, sum, add;
 	char *a, *b;
 
-	(n1 > n2)?(a = n1, b = n2):(a = n2, b = n1);
-
-	for (i = 0; a[i] != '\0'; i++)
+	for (i = 0; n1[i] != '\0'; i++)
 		;
 
-	for (j = 0; b[j] != '\0'; j++)
+	for (j = 0; n2[j] != '\0'; j++)
 		;
 
-	for (k = 0; k < size_r; k++)
-		r[k] = 48;
+	(i > j) ? (a = n1, b = n2) : (a = n2, b = n1);
 
-	for (x = i - 1, y = j - 1, z = 0; x >= 0 && y >= 0; x--, y--, z++)
+	for (x = i - 1, y = j - 1, z = 0, add = 0; x >= 0 || add != 0; x--, y--, z++)
 	{
-		sum = (a[x] - 48) + (b[y] - 48) + (r[z] - 48);
+		sum = (x >= 0) ? ((a[x] - 48) + add) : add;
+		sum = (y >= 0) ? ((b[y] - 48) + sum) : sum;
 
 		if (sum <= 9)
-			r[z] = sum + 48;
+		{
+			add = 0;
+			r[x + 1] = sum + 48;
+		}
 		else
 		{
-			if (z + 1 < size_r)
-			{
-				tmp = sum;
-				sum %= 10;
-				tmp /= 10;
-
-				r[z] = sum + 48;
-				r[z + 1] = tmp + 48;
-			}
-			else
-			{
-				i = 0;
-				break;
-			}
+			add = sum;
+			sum %= 10;
+			add /= 10;
+			r[x + 1] = sum + 48;
 		}
+		/*printf("| %d %d | %c %c | %d %d |\n", x, y, a[x], b[y], add, sum);*/
 	}
-	r[z + 1] = '\0';
-
-	for (x = 0; r[x] != '\0'; x++)
-		;
-
-	for (y = 0; y < x - 1; y++)
-	{
-		for (k = y + 1; k > 0; k--)
-		{
-			tmp = r[k];
-			r[k] = r[k - 1];
-			r[k - 1] = tmp;
-		}
-	}
-
-	if (x < size_r)
-		return (r);
-	else
+	r[z] = '\0';
+	if (z >= size_r)
 		return (0);
 
+	return (r);
 }
