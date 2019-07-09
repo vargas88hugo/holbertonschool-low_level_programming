cd49bbc609f5b2d0887dada7849ee35faf792463
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-09 02:30:08 +0000

    file 4 fixed

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,21 +8,21 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j, count = 0;
+	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *(s + i); i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		if (*(s + i) == '\0')
+			return ('\0');
+
+		for (j = 0; *(accept + j); j++)
 		{
-			if (s[i] == accept[j])
+			if (*(s + i) == *(accept + j))
 			{
-				count++;
-				break;
+				return (s + i);
 			}
 		}
-		if (count == 1)
-			break;
 	}
 
-	return (s + i);
+	return (s);
 }
