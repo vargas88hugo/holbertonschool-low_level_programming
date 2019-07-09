32d2ef2c8eabde19b6f2d1060b91145729754e4e
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-09 04:17:08 +0000

    file 9 added

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -3,7 +3,7 @@
 /**
  * set_string - Function that sets de value pointer of a pointer to a char
  * @s: String to be set
- * @to String to be changed
+ * @to: String to be changed
  */
 void set_string(char **s, char *to)
 {
