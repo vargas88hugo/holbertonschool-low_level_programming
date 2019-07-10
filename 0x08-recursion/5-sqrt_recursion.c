0aaab82bedf887f5a6b70a78d84e51f9af0c4ae6
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-10 20:31:26 +0000

    [Feature] file 5 added

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,31 @@
 #include "holberton.h"
 
+int _pow_recursion(int a, int b)
+{
+	if (b % (a / b) == 0)
+	{
+		if (b * (a / b) == a)
+			return (b);
+		else
+			return (-1);
+	}
+	else
+		return (_pow_recursion(a, b + 1));
+}
+
 /**
  * _sqtr_recursion - Function that returns the natural square root of a number
  * @n: Number to be valued
  * Return: The natural square
  */
-int _sqtr_recursion(int n)
+int _sqrt_recursion(int n)
 {
-	
+	if (n < 0)
+		return (-1);
+	else if (n == 1)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (_pow_recursion(n, 2));
 }
