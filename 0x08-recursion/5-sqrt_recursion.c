9f138b916ebea7ea5da022f0c607a300be118f3b
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-10 23:58:19 +0000

    [Fix] file 5 fixed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,37 +1,34 @@
 #include "holberton.h"
 
 /**
- * _pow_recursion - Function that find power number for square root
- * @a: base of the power
- * @b: exponation of the power
- * Return: Power of the number
+ * _power - Function that valued if is prime
+ * @a: Number to be valued
+ * @b: Parameter for value
+ * Return: Return the value
  */
-int _pow_recursion(int a, int b)
+int _power(int a, int b)
 {
-	if (b % (a / b) == 0)
-	{
-		if (b * (a / b) == a)
-			return (b);
-		else
-			return (-1);
-	}
+	if ((b * b) == a)
+		return (b);
+	else if ((b * b) < a)
+		return (_power(a, b + 1));
 	else
-		return (_pow_recursion(a, b + 1));
+		return (-1);
 }
 
 /**
- * _sqrt_recursion - Function that returns the natural square root of a number
+ * _sqrt_recursion - Function that returns the natural square root
  * @n: Number to be valued
- * Return: The natural square
+ * Return: The natural square root or -1 if don't have square
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 1)
-		return (1);
 	else if (n == 0)
 		return (0);
-	else
-		return (_pow_recursion(n, 2));
+	else if (n == 1)
+		return (1);
+
+		return (_power(n, 2));
 }
