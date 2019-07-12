d806cd401dbef427286297274852c3431db582da
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-12 22:35:07 +0000

    [Feature] 1-args file added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * @argc: Number of arguments passed
+ * @__attribute__: unused
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
