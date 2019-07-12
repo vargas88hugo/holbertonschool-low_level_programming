7fcb616069e46babf2519b78b4181098dabeabdc
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-12 23:11:01 +0000

    [Feature] 3-mul.c file added

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Function that multiplies two numbers
+ * @argc: Number of arguments passed
+ * @argv: Array of strings
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+
+	if (argc < 3)
+		return (1);
+
+	printf("%d\n", i * j);
+
+	return (0);
+}
