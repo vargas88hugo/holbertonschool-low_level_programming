d5ec1c099a00aa2f11327369e9c51ca8fdb976fe
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-15 00:50:59 +0000

    [Feature] 100-change.c file added

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define LEN 5
+
+/**
+ * main - Entry Point
+ * @argc: Number of arguments passed
+ * @argv: Array of strings
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, count = 0;
+	int x = atoi(argv[1]);
+	int y[LEN] = {1, 2, 5, 10, 25};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = LEN - 1; i >= 0; i--)
+	{
+		if (x - y[i] >= 0 && x)
+		{
+			x -= y[i];
+			i++, count++;
+		}
+	}
+
+	printf("%d\n", count);
+
+	return (0);
+}
