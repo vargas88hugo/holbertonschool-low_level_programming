8beb4fd657beff249d284d4a20fd82e6e6cc33f8
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-16 19:09:37 +0000

    [Fix] 0-create_array betty repared and declaration of null

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,10 +17,11 @@ char *create_array(unsigned int size, char c)
 
 	s = malloc(size * sizeof(char));
 
-	for (i = 0; i < size; i++)
-		s[i] = c;
 	if (s == NULL)
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+		s[i] = c;
 
 	return (s);
 }
