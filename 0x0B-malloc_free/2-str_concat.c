7383432eed950fe9567b02497c708e2e204d4a4d
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-16 20:36:03 +0000

    [Fix] 2-str_concat.c file repared, missing conversion from NULL to empty string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,8 +32,10 @@ char *str_concat(char *s1, char *s2)
 	for (x = 0; x < i; x++)
 		p[x] = s1[x];
 
-	for (y = i, z = 0; y < i + j + 1; y++, z++)
+	for (y = i, z = 0; y < i + j; y++, z++)
 		p[y] = s2[z];
+
+	p[i + j] = '\0';
 
 	return (p);
 }
