849dde267468571d36cfec3809e4375d703a0243
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-16 21:51:23 +0000

    [Feature] 3-alloc_grid.c file has been created

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,13 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **a;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
 	a = malloc(sizeof(int *) * width);
+
+	if (a == NULL)
+		return (NULL);
 
 	for (i = 0; i < width; i++)
 	{
