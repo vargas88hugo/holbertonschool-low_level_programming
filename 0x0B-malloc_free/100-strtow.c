ec403a7cdb187adece3cb53f5067a472f267990a
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-17 04:58:43 +0000

    [Feature] 100-strtow.c file has been created

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * strtow - Function that splits a string into words
+ * @str: String to be worked
+ * Return: Pointer to an array of strings
+ */
+char **strtow(char *str)
+{
+	int i, j, k = 0 ,c = 0;
+	char **a;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' && str[i + 1] != ' ')
+			c++:
+	}
+
+	a = malloc(sizeof(char *) + c);
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 33 && str[i] <= 126)
+		{
+			for (j = 0; str[i] != '\0'; i++)
+				a[k][j] = str[i];
+
+			k++;
+		}
+	}
+
+	return (a);
+}
