20c55c66df19e8f546c0906b9a04b88e6a226437
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-22 20:15:42 +0000

    [Feature] 2-print_dog.c file has been added

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,16 +13,25 @@ void print_dog(struct dog *d)
 		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: (nil)\n");
+		{
+			d->name = "(nil)";
+			printf("Name: %s\n", d->name);
+		}
 
 		if (d->age != 0)
 			printf("Age: %f\n", d->age);
 		else
-			printf("Age: (nil)\n");
+		{
+			d->age = 0;
+			printf("Age: %s\n", d->age);
+		}
 
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
+		{
+			d->owner: "(nil)";
 			printf("Owner: (nil)\n");
+		}
 	}
 }
