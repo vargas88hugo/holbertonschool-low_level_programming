bb4edf2680b757861514d40f885d98b167631083
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-22 18:21:23 +0000

    [Feature] dog.h header has been created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - This struct is a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Name of the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
