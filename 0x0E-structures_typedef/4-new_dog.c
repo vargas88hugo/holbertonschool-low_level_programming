68014c836545da6fd0ae5f4d484bc232bbfd9784
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-22 22:46:23 +0000

    [Feature] new_dog.c file has been created

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,6 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 
 	if (d == NULL)
+		free(d);
 		return (NULL);
 
 	for (i = 0; name[i] != '\0'; i++)
@@ -27,6 +28,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d->name = malloc(sizeof(char) + (i + 1));
 
 	if (d->name == NULL)
+		free(d->name);
 		return (NULL);
 
 	for (x = 0; x <= i; x++)
@@ -37,6 +39,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d->owner = malloc(sizeof(char) + (j + 1));
 
 	if (d->owner == NULL)
+		free(d->owner);
 		return (NULL);
 
 	for (y = 0; y <= j; y++)
