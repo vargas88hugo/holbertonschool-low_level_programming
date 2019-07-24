dbd6b916fa208340ecec9d71a7cd7195cc12c575
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-24 17:27:38 +0000

    [Feature] 0-print_name.c has been created

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Function that prints a name
+ * @name: Name of the person
+ * @f: Function as a parameter
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
