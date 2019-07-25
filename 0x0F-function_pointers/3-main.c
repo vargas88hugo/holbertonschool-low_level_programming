e373b8063564d69b4049e4c6652009c5839d2a99
Hugo Vargas <vargas88hugo@gmail.com>
2019-07-24 23:49:57 -0500

    [Fix] Wrong code exit error

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 	if ((argv[2][0] == '%' || argv[2][0] == '/') && argv[3][0] == '0')
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
 
 	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
