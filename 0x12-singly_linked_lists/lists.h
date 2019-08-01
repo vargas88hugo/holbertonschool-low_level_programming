46284648e5e3b469d3f33e2762158a6d0f7ae3ed
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-01 13:58:41 -0500

    [feature] lists.h header of the project has been created

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LIST_H_
+#define LIST_H_
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+
+#endif
