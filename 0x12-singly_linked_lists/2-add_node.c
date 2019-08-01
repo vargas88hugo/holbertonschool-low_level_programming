673f7a36c9b16aff14fcc697e7237cb6e7bc7634
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-01 18:28:36 -0500

    [fix] 2-add_node, issue with the free and malloc

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,34 +11,17 @@ list_t *add_node(list_t **head, const char *str)
 	int i;
 	list_t *new;
 
-	if (head == NULL || str == NULL)
-	{
-		free(head);
-		return (NULL);
-	}
-
 	for (i = 0; str[i]; i++)
 		;
 
 	new = (list_t *)malloc(sizeof(list_t));
 	if (new == NULL)
-	{
-		free(head);
 		return (NULL);
-	}
-
-	new = (list_t *)malloc(sizeof(char) * (i + 1));
-	if (new == NULL)
-	{
-		free(new);
-		free(new);
-		return (NULL);
-	}
 
 	new->str = strdup(str);
 	new->next = (*head);
 
 	(*head) = new;
 
-	return (new);
+	return (*head);
 }
