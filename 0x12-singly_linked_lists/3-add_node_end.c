d80a1e4a8a3857766b475dc019259ebd23d2bc71
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-01 20:57:42 -0500

    [fix] 3-add_node_end.c had a problem with its return

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,33 +1,41 @@
 #include "lists.h"
 
 /**
- * add_node_end - Function that adds a new node at the end
- * @head: Beginning of the list
- * @str: String to be stored
+ * add_node_end - Function that adds a new node at the end of the list
+ * @head: beginning node of the list
+ * @str: string to be stored
  * Return: The adress of the new element
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new = malloc(sizeof(list_t));
+	int i;
+	list_t *new = NULL;
+	list_t *last = NULL;
+
+	for (i = 0; str[i]; i++)
+		;
+
+	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
+	new->len = i;
 	new->next = NULL;
-	list_t *last = (*head);
 
-	if (*head == NULL)
+	last = (*head);
+
+	if (*head)
 	{
-		(*head) = new;
-		return (*head);
-	}
-	else
-	{
-		while (last->next != NULL)
+		while (last->next)
 			last = last->next;
 
 		last->next = new;
+	}
+	else
+	{
+		(*head) = new;
 	}
 
 	return (*head);
