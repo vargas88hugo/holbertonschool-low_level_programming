0f3930b34020087dee2565693304303afdcc21d4
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-05 20:01:23 +0000

    [feature] 5-free_listint2.c created

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Function that frees a list and equal to NULL
+ * @head: Beginning of the list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = NULL;
+
+	while (*head)
+	{
+		temp = (*head);
+		(*head) = (*head)->next;
+		free(temp);
+	}
+
+	(*head) = NULL;
+}
