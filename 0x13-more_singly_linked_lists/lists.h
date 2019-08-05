612a2eed5361f61d75cd918249dbc36ec346c103
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-05 15:20:48 +0000

    [feature] lists.h header created

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H_
+#define LISTS_H_
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+int _putchar(char c);
+size_t print_listint(const listint_t *h);
+
+#endif
