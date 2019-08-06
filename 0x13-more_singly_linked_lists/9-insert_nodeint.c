75e7956f35eb54ddac05694442e98e1b58a2d263
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-06 17:34:04 +0000

    [fix] catch error was not well configured, missing case at the beginning and at the end added node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,11 +21,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new->n = n;
 
-	for (i = 0; i < idx - 1; i++)
+	if (idx == 0)
+	{
+		new->next = temp;
+		temp = new;
+		return (*head);
+	}
+
+	for (i = 1; i < idx; i++)
 	{
 		if (temp == NULL)
 			return (NULL);
 
+		if (temp->next == NULL && i == idx - 2)
+		{
+			temp->next = new;
+			new->next = NULL;
+			return (*head);
+		}
 		temp = temp->next;
 	}
 
