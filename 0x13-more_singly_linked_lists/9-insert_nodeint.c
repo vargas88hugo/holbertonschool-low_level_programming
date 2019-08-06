614d6cda6487f662d381777950e54933515a16d9
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-06 19:45:29 +0000

    [fix] issue in the cases added nodes at the beginning and at the end

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
-	if ((*head) == NULL)
+	if ((*head) == NULL && idx == 0)
 		return (new);
 
 	if (idx == 0)
