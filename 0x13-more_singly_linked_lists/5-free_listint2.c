4137b8597be00a129a00fb9dbd6a42b35157208d
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-06 15:48:24 +0000

    [fix] missing double pointer declaration

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = NULL;
 
-	while (**head && *head)
+	while (head != NULL && (*head) != NULL)
 	{
 		temp = (*head);
 		(*head) = (*head)->next;
 		free(temp);
 	}
-
-	(*head) = NULL;
 }
