8393e94875f0e9d5c612148afb60603ba3cf779a
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-08 18:36:22 +0000

    [feature] 0-binary_to_uint.c crated and added in the header

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - Function that converts a binary number to decimal
+ * @b: Binary number in a string
+ * Return: The converted number or 0 if fail
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int res = 0, i, j = 1, k;
+
+	for (k = 0; b[k] != '\0'; k++)
+		;
+
+	for (i = k - 1; i >= 0; i--, j *= 2)
+	{
+		if (b[i] < 48 || b[i] > 57)
+			return (0);
+
+		if (b[i] & 1)
+			res += j;
+	}
+
+	return (res);
+
+}
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
