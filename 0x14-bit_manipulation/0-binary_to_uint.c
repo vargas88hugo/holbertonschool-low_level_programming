121b1d05e8e82a6fea0e2f5c4ad199c8dc98cb1a
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-08 18:54:03 +0000

    [fix] it was not configured for when it exceeds the number of bytes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = k - 1; i >= 0; i--, j *= 2)
 	{
-		if (b[i] != 48 && b[i] != 49)
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 
 		if (b[i] & 1)
@@ -23,5 +23,4 @@ unsigned int binary_to_uint(const char *b)
 	}
 
 	return (res);
-
 }
