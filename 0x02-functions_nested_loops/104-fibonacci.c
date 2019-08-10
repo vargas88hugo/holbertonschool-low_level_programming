d0e3ab3177bd25fb374b12a24eb2b2423daae9e7
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-10 13:19:10 -0500

    [fix] it was an error en the last number, I had to divide the variables

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,26 +7,40 @@
  */
 int main(void)
 {
-	int i;
-	long int a = 1, b = 2, c;
+	unsigned long int i, a = 1, b = 2, c, c1, a1, b1, a2, b2;
 
 	for (i = 1; i <= 98; i++)
 	{
-		if (i <= 2)
+		if (i < 93)
 		{
-			c = i;
+			if (i <= 2)
+			{
+				c = i;
+			}
+			else
+			{
+				c = a + b;
+				a = b;
+				b = c;
+			}
+			printf("%lu", c);
 		}
 		else
 		{
-			c = a + b;
-			a = b;
-			b = c;
+			if (i == 93)
+			{
+				a1 = a % 1000000000;
+				a /= 1000000000;
+				b1 = b % 1000000000;
+				b /= 1000000000;
+			}
+			c1 = a1 + b1, c = a + b;
+			a = b, a1 = b1;
+			b = c, b1 = c1;
+			printf("%lu%lu", c, c1);
 		}
-		printf("%ld", c);
-		if (i < 50)
-		{
+		if (i < 98)
 			printf(", ");
-		}
 	}
 	putchar(10);
 
