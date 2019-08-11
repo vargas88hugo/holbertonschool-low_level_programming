7165721c946244aa6fee952d1be35fbe2b977d63
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-11 18:06:48 -0500

    [fix] finally it is works

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -8,22 +8,14 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 != *s1 && *s2 == '*')
-	{
-		if (*(s2 + 1) == '*')
-			return (wildcmp(s1, s2 + 1));
-		else if (*(s2 + 1) != *(s2 + 1))
-			return (wildcmp(s1 + 1, s2));
-		else if (*s1 == *s2)
-			return (wildcmp(s1 + 1, s2 + 1));
-		else if (*s2 == '\0' || *s1 == '\0')
-			return (1);
-	}
-	else if (*s2 != *s1 && *s2 != '*')
+	if (*s1 != *s2 && *s2 != '*')
 		return (0);
-	else if (*s2 == '\0')
+	else if (*s1 == '\0' && *s2 == '\0')
 		return (1);
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2+ 1));
 
-
-	return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
