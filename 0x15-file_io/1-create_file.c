1057530c789b6622f2ee3411205f251a14081147
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-13 14:51:43 -0500

    [fix] missing catch error of write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,6 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content[0] = '\0';
-
 	/* write */
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
@@ -25,7 +22,13 @@ int create_file(const char *filename, char *text_content)
 	for (i = 0; *(text_content + i); i++)
 		;
 
-	write(fd, text_content, i);
+	if (text_content != NULL)
+		i = write(fd, text_content, i);
+	else
+		i = write(fd, "", 0);
+
+	if (i == -1)
+		return (-1);
 
 	close(fd);
 
