ce6df2063bf97e4e570114aad15df80cdbedc5d6
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-13 12:33:21 -0500

    [feature] header and gitignored created

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
