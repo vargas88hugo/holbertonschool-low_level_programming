7a63a80949ceeedfcee8fd48c33008fb194ad980
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-13 23:51:46 -0500

    [feature] 3-cp.c created

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,26 +13,26 @@ int main(int argc, char **argv)
 	char buff[BUFFER];
 
 	if (argc != 3)
-		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	if (argv[1] == NULL)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[0]), exit(98);
 	/* read */
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[0]), exit(98);
 	bt = read(fd, buff, BUFFER);
 	buff[bt] = '\0';
 	cl = close(fd);
 	if (cl == -1)
-		dprintf(2, "Error: Can't close fd %d\n", bt), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", bt), exit(100);
 	/* write */
 	fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0665);
 	if (fd == -1)
-		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]), exit(99);
 	write(fd, buff, bt);
 	cl = close(fd);
 	if (cl == -1)
-		dprintf(2, "Error: Can't close fd %d\n", bt), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", bt), exit(100);
 
 	return (0);
 }
