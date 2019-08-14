04d6f5292296ec00424de11e01b6169ebaf5b64a
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-14 13:35:21 -0500

    [fix] missing catch error about read

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char **argv)
 {
-	int fd1, fd2, bt = BUFFER;
+	int fd1, fd2, bt1 = BUFFER, bt2;
 	char buff[BUFFER];
 
 	if (argc != 3)
@@ -22,19 +22,17 @@ int main(int argc, char **argv)
 	}
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while (bt1 > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	while (bt > 0)
-	{
-		bt = read(fd1, buff, BUFFER);
-		if (bt == -1)
+		bt1 = read(fd1, buff, BUFFER);
+		if (bt1 == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		if (write(fd2, buff, bt) != bt)
+		bt2 = write(fd2, buff, bt1);
+		if (bt2 == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
