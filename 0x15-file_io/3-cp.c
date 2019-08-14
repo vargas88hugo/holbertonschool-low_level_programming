927162efb2d3eb7ef17e36a0312900c33f84e3dc
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-14 00:14:10 -0500

    [feature] 3-cp.c created

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,18 +22,18 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 	bt = read(fd, buff, BUFFER);
-	buff[bt] = '\0';
 	cl = close(fd);
 	if (cl == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", bt), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", bt), exit(99);
 	/* write */
 	fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
 	if (fd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]), exit(99);
-	write(fd, buff, bt);
+	while (bt != 0)
+		write(fd, buff, bt);
 	cl = close(fd);
 	if (cl == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", bt), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", bt), exit(99);
 
 	return (0);
 }
