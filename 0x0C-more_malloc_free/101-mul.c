43b6dd96d326aa88f33597437785cea8cc01585d
Hugo Vargas <vargas88hugo@gmail.com>
2019-08-15 10:31:54 -0500

    [feature] 101-mul.c created, the file is empty

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * main - Program that mulplies two positive numbers
+ * @argc: Number of argumenets
+ * @argv: Muldimensional array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+
+}
