95b2a6485c9e6c38efd30fce4ec98f03a5889ece
Hugo Vargas <vargas88hugo@gmail.com>
2019-09-16 18:02:16 -0500

    [fix] missing slash in the directory

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/vargas88hugo/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/hack.so
+wget -P /tmp/ https://github.com/vargas88hugo/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/hack.so
 export LD_PRELOAD=/tmp/hack.so
