66b6edebe0fd5366e50d636f327b67db6627fffa
Hugo Vargas <vargas88hugo@gmail.com>
2019-10-11 19:54:13 +0000

    [feature] created 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,36 +9,52 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	int index;
-	hash_node_t *new;
+	int index, node;
+	hash_node_t *new = NULL, *tmp = NULL;
 
 	if (ht == NULL || key == NULL || value == NULL)
-		return (NULL);
+		return (0);
 
-	index = key_index(key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (ht->array[index] != NULL)
+	{
+		node = search_collision(ht, (char *)key, (char *)value, index);
+		if (node == 1)
+			return (1);
+	}
 
 	new = create_node(key, value);
+	if (new == NULL)
+		return (0);
 
 	if (ht->array[index] == NULL)
 	{
-		new->next = NULL;
+		ht->array[index] = new;
 		return (1);
 	}
+
+	tmp = ht->array[index];
+	ht->array[index] = new;
+	new->next = tmp;
+
+	return (1);
 }
 
 /**
  * create_node - function that creates a node
  * @key: key of the node of the hash table
  * @value: value of the node of the hash table
+ * @key: key of the node of the hash table
  * Return: the created new node
  */
 hash_node_t *create_node(const char *key, const char *value)
 {
-	hash_node_t *new;
+	hash_node_t *new = NULL;
 
 	new = malloc(sizeof(hash_node_t) * 1);
 	if (new == NULL)
-		return NULL;
+		return (NULL);
 
 	new->key = strdup(key);
 	if (new->key == NULL)
@@ -47,9 +63,41 @@ hash_node_t *create_node(const char *key, const char *value)
 	new->value = strdup(value);
 	if (new->value == NULL)
 	{
-		free(new-key);
+		free(new->key);
 		return (NULL);
 	}
 
+	new->next = NULL;
+
 	return (new);
+}
+
+/**
+ * search_collision - function that cheks if there is a collission
+ * @ht: hash table
+ * @key: key of the node of the hash table
+ * @value: value of the node of the hash table
+ * @index: index of the hash
+ * Return: 1 on success and 0 otherwise
+ */
+int search_collision(hash_table_t *ht, char *key, char *value, int index)
+{
+	hash_node_t *tmp = NULL;
+
+	tmp = ht->array[index];
+	while (tmp)
+	{
+		if (strcmp(key, tmp->key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			if (tmp->value == NULL)
+				exit(1);
+
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+
+	return (0);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -41,5 +41,6 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 hash_node_t *create_node(const char *key, const char *value);
+int search_collision(hash_table_t *ht, char *key, char *value, int index);
 
 #endif
