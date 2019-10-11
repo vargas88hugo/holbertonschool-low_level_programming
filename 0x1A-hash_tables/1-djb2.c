e948b9d0781565630282fcce6ebceeee0ebbcad5
Hugo Vargas <vargas88hugo@gmail.com>
2019-10-11 00:30:56 +0000

    [feature] created 1-djb2.c

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - djb2 algorithm function
+ * @str: string to be hashed
+ * Return: hash of string
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
+}
