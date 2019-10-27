45a172ac86ef7d0bb8e6c467bb52988db43f115e
Hugo Vargas <vargas88hugo@gmail.com>
2019-10-27 17:44:39 -0500

    [feature] created 1-insertion_sort_list.c

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -13,14 +13,16 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void swap(int *a1, int *a2);
+void insertion_sort_list(listint_t **list);
+void swap_list(listint_t **n1, listint_t **n2);
 
 #endif
