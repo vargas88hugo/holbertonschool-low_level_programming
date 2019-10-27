51636afc2a220df013f45ff814f694eed62d65b4
Hugo Vargas <vargas88hugo@gmail.com>
2019-10-27 18:06:01 -0500

    [feature] created 2-selection_sort.c

---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+
+/**
+ * swap - function that swaps two values of an array
+ * @a1: first value
+ * @a2: second value
+ */
+void swap(int *a1, int *a2)
+{
+	int temp;
+
+	temp = *a1;
+	*a1 = *a2;
+	*a2 = temp;
+}
+
+/**
+ * selection_sort - Selection Sort Algorithm
+ * @array: Array to be sorted
+ * @size: Size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		k = i;
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < array[k])
+				k = j;
+		}
+		swap(&array[k], &array[i]);
+		print_array(array, size);
+	}
+}
