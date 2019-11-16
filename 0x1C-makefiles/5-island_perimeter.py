58cdf37eb61b2e36295f520391f54fa566f1fefb
Hugo Vargas <vargas88hugo@gmail.com>
2019-11-15 20:37:22 -0500

    [feature] fixex perimeter calculation when the island is on the limit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,18 +3,17 @@
 
 
 def island_perimeter(grid):
-    """ method that returns the perimeter of the island grid """
     per = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i - 1][j] == 0:
+                if i == 0 or grid[i - 1][j] == 0:
                     per += 1
-                if grid[i + 1][j] == 0:
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
                     per += 1
-                if grid[i][j - 1] == 0:
+                if j == 0 or grid[i][j - 1] == 0:
                     per += 1
-                if grid[i][j + 1] == 0:
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
                     per += 1
 
     return per
